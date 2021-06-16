7de2d5f0690a6ab0505ed83a4f855b55282c84c6
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2021-06-16 09:26:18 +0000

    Cambio en el parseo de las incidencias 'fallo en llamada de test'..
    git-svn-id: http://10.34.58.18/svn/svn_repository/Ulises5000I/branches/agl-dev/ulises-man@9392 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

--- Pruebas/UnitTesting/GwCallLogsParseTest.cs
+++ Pruebas/UnitTesting/GwCallLogsParseTest.cs
@@ -23,16 +23,22 @@ namespace UnitTesting
          * */
         readonly List<string> IncisFromGateways = new List<string>()
         {
-            "2007:2015-11-06 11.07.47:EMPLAZ:CGW3:31:-",
-            "2005:2015-11-06 11.07.39:EMPLAZ:CGW3:19:-:192.168.0.223:Principal:Reserva",
-            "2007:2015-11-06 11.07.47:EMPLAZ:CGW3:31:-:TX1",
-            "2008:2015-11-06 11.07.43:EMPLAZ:CGW3:23:-:TX1",
-            "2009:2015-11-06 11.07.44:EMPLAZ:CGW3:26:-:(AB) AB-2",
-            "2010:2015-11-06 11.07.44:EMPLAZ:CGW3:24:-:(AB) AB-2",
-            "2101:2015-11-06 11.07.50:EMPLAZ:CGW3:33:-:RX1:ON:sip.UV5KI@10.12.60.133:Activa",
-            "2204:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:R2-1",
-            "2311:2015-11-06 10.58.12:EMPLAZ:CGW3:17:-:AB-1:IF=4|DS=RTB:06/11/2015 10.58.12",
-            "2312:2015-11-06 11.02.52:EMPLAZ:CGW3:18:-:AB-1:IF=4|DS=RTB|DL=0000280:06/11/2015 11.02.52",
+            //"2007:2015-11-06 11.07.47:EMPLAZ:CGW3:31:-",
+            //"2005:2015-11-06 11.07.39:EMPLAZ:CGW3:19:-:192.168.0.223:Principal:Reserva",
+            //"2007:2015-11-06 11.07.47:EMPLAZ:CGW3:31:-:TX1",
+            //"2008:2015-11-06 11.07.43:EMPLAZ:CGW3:23:-:TX1",
+            //"2009:2015-11-06 11.07.44:EMPLAZ:CGW3:26:-:(AB) AB-2",
+            //"2010:2015-11-06 11.07.44:EMPLAZ:CGW3:24:-:(AB) AB-2",
+            //"2101:2015-11-06 11.07.50:EMPLAZ:CGW3:33:-:RX1:ON:sip.UV5KI@10.12.60.133:Activa",
+            //"2204:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:R2-1",
+            //"2311:2015-11-06 10.58.12:EMPLAZ:CGW3:17:-:AB-1:IF=4|DS=RTB:06/11/2015 10.58.12",
+            //"2312:2015-11-06 11.02.52:EMPLAZ:CGW3:18:-:AB-1:IF=4|DS=RTB|DL=0000280:06/11/2015 11.02.52",
+
+            //"2204:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:r2-1",
+            //"2205:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:r2-1",
+            //"2207:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:n2-1",
+            //"2208:2015-11-06 10.58.11:EMPLAZ:CGW3:16:-:n2-1",
+            "2304:2021-06-16 07.57.48:EMPLAZ:CGW3:39:-:n5-n2:RC=n5-n2:16/06/2021 07.57.48",
         };
         [TestMethod]
         public void TestMethod1()
@@ -41,7 +47,7 @@ namespace UnitTesting
             {
                 new Redan2UlisesHist(inciText).UlisesInci((inci, parametros) =>
                 {
-                    Debug.WriteLine($"{inci.ToString()}: {String.Join(", ", parametros.ToArray())}");
+                    Debug.WriteLine($"{inci}: {String.Join(", ", parametros.ToArray())}");
                 }, () =>
                 {
                     Debug.WriteLine($"GWU-HISTORICO NO CONVERTIDO: <<<{inciText}>>>");
